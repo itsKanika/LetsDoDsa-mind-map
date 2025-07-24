2e935f87e26551d3638aab209b72a159b03922d4
Unknown Guy <91245369+unknownguy49@users.noreply.github.com>
2025-07-24 17:19:45 +0530

    added theme toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import './darkmode.css';
+import { ThemeProvider } from './components/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
