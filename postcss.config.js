708c4a5ca057a14b5ee2ccf6bf56a2aab79eefa2
Vaishnavi-Manne <mannevaishnavi16@gmail.com>
2025-08-04 23:19:02 +0530

    updated files to revert commits

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+import tailwindcss from '@tailwindcss/postcss'; // Correct import for the PostCSS plugin
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-  tailwindcss: {},
-  autoprefixer: {},
-  },
-}
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
