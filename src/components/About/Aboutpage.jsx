d8829855b0a2eb58a7979947d60ac5dee21b08f1
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2025-08-13 16:53:50 +0530

    Fixed padding issue in about page and contact page

--- src/components/About/Aboutpage.jsx
+++ src/components/About/Aboutpage.jsx
@@ -19,7 +19,7 @@ const Aboutpage = () => {
   return (
     <div id="about" className="about bg-gradient-to-b from-[#e0f2fe] via-[#cfe0fc] to-[#dbeafe] font-sans pt-10">
       <div className="container mx-auto px-4 py-8 md:py-12">
-        <header className="text-center mb-10 md:mb-16">
+        <header className="text-center mt-5 mb-10 md:mb-16">
           <h1 className="text-4xl md:text-5xl font-extrabold text-indigo-700 dark:text-indigo-400 mb-4">
             Lets's Do DSA Together
           </h1>
